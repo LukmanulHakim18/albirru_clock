8748c3c24bbe313aeb55b89ddb881b4c24ef1dde
lukmanul.hakim <lukmanul.hakim@bluebirdgroup.com>
2024-10-24 01:45:27 +0700

    finishing code

--- albirru_clock.ino
+++ albirru_clock.ino
@@ -4,6 +4,86 @@
 #include <TM1637Display.h>
 #include <Servo.h>
 
+
+// ================================ oop start ================================
+
+const char NIGHT = 'N';
+const char DAY = 'D';
+
+class BirruClock{
+private:
+   void setDayNightCycle(){
+      if (Hour >= 6 && Hour >= 17){
+         DayNightCycle = NIGHT;
+         Serial.println("set DayNightCycle to Night");
+      }else{
+         DayNightCycle= DAY;
+         Serial.println("set DayNightCycle to Day");
+      };
+   };
+
+   void setAngleOfServo(){
+      if (DayNightCycle == NIGHT){
+         if (AngleOfServo < 180){
+            AngleOfServo++;
+            Serial.println("set AngleOfServo ++");
+         }
+      }else{
+         if (AngleOfServo > 0){
+            AngleOfServo--;
+            Serial.println("set AngleOfServo ++");
+         };
+      };
+   };
+
+   void setBrightness(){
+      if (DayNightCycle == NIGHT){
+         if (DisplayBrightness > 1){
+            DisplayBrightness--;
+            Serial.println("set DayNightCycle --");
+         }
+      }else{
+         if (DayNightCycle < 7 ){
+            DayNightCycle++;
+            Serial.println("set DayNightCycle ++");
+         };
+      };
+   };
+
+public: 
+   int Hour;
+   int Minute;
+   int DisplayBrightness;
+
+   char DayNightCycle;
+
+   int AngleOfServo;
+   
+   void setupDefault(){
+      AngleOfServo = 0;
+      DisplayBrightness = 7;
+   }
+   void Update(int hh ,int mm ){
+      Hour = hh;
+      Minute = mm;
+      DayNightCycle = DAY;
+      
+      Serial.println("masuk ke Update");
+      setDayNightCycle();
+      setAngleOfServo();
+      setBrightness();
+
+      Serial.println(DisplayBrightness);
+      Serial.println(DayNightCycle);
+      Serial.println(AngleOfServo);
+      
+   };
+};
+
+// ================================ oop end ================================
+
+
+
 // Declarated pin
 #define CLK D3   
 #define DIO D4
@@ -11,11 +91,11 @@
 
 // Declarated Object
 TM1637Display display = TM1637Display(CLK, DIO);
-Servo setupDefault(); 
+Servo myServo; 
 BirruClock myClock;
 
-const char *ssid     = "Birrur";
-const char *password = "14072022";
+String ssid = "";
+String password = "";
 
 const long utcOffsetInSeconds = 25200;
 
@@ -38,25 +118,53 @@ NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", utcOffsetInSeconds);
 int hh, mm, ss, timeToDisplay;
 
 void setup() {
+   pinMode(LED_BUILTIN, OUTPUT);  
+   digitalWrite(LED_BUILTIN, LOW);
    myClock.setupDefault();
    Serial.begin(115200);
-   display.setBrightness(7);
+
+   display.setBrightness(myClock.DisplayBrightness);
    timeClient.begin(); 
+
+   myServo.write(myClock.AngleOfServo);
+   
+   Serial.println("Please input your Wi-Fi SSID:");
+   while (ssid == "") {
+    if (Serial.available()) {
+      ssid = Serial.readStringUntil('\n');
+      ssid.trim();  // Remove any extra whitespace characters
+      Serial.print("SSID set to: ");
+      Serial.println(ssid);
+      delay(500);
+    }
+  }
+  Serial.println("Please input your Wi-Fi Password:");
+  while (password == "") {
+    if (Serial.available()) {
+      password = Serial.readStringUntil('\n');
+      password.trim();  // Remove any extra whitespace characters
+      Serial.print("Password set to: ");
+      Serial.println(password);
+      delay(500);
+    }
+  }
 
    WiFi.begin(ssid, password);
 
    while ( WiFi.status() != WL_CONNECTED ) {
       display.setSegments(aboy);
-      delay ( 500 );
+      
+      digitalWrite(LED_BUILTIN, LOW); 
+      delay ( 5000 );
       display.clear();
-      Serial.print ( "." );
+      Serial.print ( "." );      
+      digitalWrite(LED_BUILTIN, HIGH);
    }
-
-   pinMode(LED_BUILTIN, OUTPUT);
+   digitalWrite(LED_BUILTIN, HIGH); 
 }
 
 void loop() {
-   
+ 
    timeClient.update();
    hh = timeClient.getHours();
    mm = timeClient.getMinutes();
@@ -71,89 +179,13 @@ void loop() {
    Serial.println(ss);
    Serial.println(timeClient.getFormattedTime());
 
-   if (mm%2==0){
-      display.setBrightness(7);
-      for(int i=0;i<hh; i++){
-         display.setSegments(aboy);
-         delay(500);
-         display.clear();
-         delay(500);
-         }   
-      }else{
-         display.setBrightness(1);
-         }
+   myClock.Update(hh, mm);
+   display.setBrightness(myClock.DisplayBrightness);
+   myServo.write(myClock.AngleOfServo);
    
-   timeToDisplay = (hh*100)+mm;
-   
+   timeToDisplay = (hh*100) + mm;
    display.showNumberDecEx(timeToDisplay, 0b11100000, false, 4, 0);
-   //  digitalWrite(LED_BUILTIN, HIGH);
    delay(500);
-
    display.showNumberDecEx(timeToDisplay,true);  
-   //  digitalWrite(LED_BUILTIN, LOW);
-   delay(500);
+   delay(500); 
 }
-
-
-// ================================ oop ================================
-
-const char NIGHT = 'NIGHT';
-const char DAY = 'DAY';
-
-class BirruClock{
-private:
-   void setDayNightCycle(){
-      if (Hour >= 17){
-         DayNightCycle = NIGHT;
-      }else{
-         DayNightCycle= DAY;
-      };
-   };
-
-   void setAngleOfServo(){
-      if (DayNightCycle == NIGHT){
-         if (AngleOfServo < 180){
-            AngleOfServo++;
-         }
-      }else{
-         if (AngleOfServo > 0){
-            AngleOfServo--;
-         };
-      };
-   };
-
-   void setBrightness(){
-      if (DayNightCycle == NIGHT){
-         if (DisplayBrightness < 7){
-            DisplayBrightness--;
-         }
-      }else{
-         if (AngleOfServo > 0){
-            AngleOfServo--;
-         };
-      };
-   };
-
-public: 
-   int Hour;
-   int Minute;
-   int DisplayBrightness;
-
-   char DayNightCycle;
-
-   int AngleOfServo;
-   
-   void setupDefault(){
-      AngleOfServo = 0;
-      DisplayBrightness = 7;
-   }
-   void Update(int hh ,int mm ){
-      Hour = hh;
-      Minute = mm;
-      setDayNightCycle();
-      setAngleOfServo();
-   };
-};
-
-
-// class String{};
